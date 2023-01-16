7a140c1f92d758320f3fdafe6cf17a7a84a40437
Maria Volintiru <maria.volintiru@yahoo.com>
2023-01-16 20:30:14 +0200

    3

--- Models/Restaurant View Models/DeliveryFoodData.cs
+++ Models/Restaurant View Models/DeliveryFoodData.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication_Oltean_Cristina.Models.RestaurantViewModels
+namespace WebApplication_Volintiru.Models.RestaurantViewModels;
+
+public class DeliveryFoodData
 {
-    public class DeliveryFoodData
-    {
-        public int FoodID { get; set; }
-        public string Name { get; set; }
-        public bool IsPublished { get; set; }
-        public bool IsDelivery { get; internal set; }
-    }
+    public int FoodID { get; set; }
+    public string Name { get; set; }
+    public bool IsPublished { get; set; }
+    public bool IsDelivery { get; internal set; }
 }
